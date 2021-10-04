9eefe0325b56d04b1a82681fbff769e7606a943e
gjdamasco <gjcenina@gmail.com>
2021-10-03 21:45:08 -0400

    contact and about pages completed

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/services', function(req, res, next) {
 
 /* GET Contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'My Contact', page: 'contact'});
+  res.render('index', { title: 'Contact Page', page: 'contact'});
 });
 
 module.exports = router;
